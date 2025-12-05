87e2e781e7dc3c508c4410717eec3fc4b19c5b96
bezmenovd <d28829192@gmail.com>
2025-12-05 22:26:30 +0500

    fix: spectator

--- pm2/dev/spectator.config.js
+++ pm2/dev/spectator.config.js
@@ -7,7 +7,7 @@ module.exports = {
             max_restarts: 3,
             restart_delay: 3000,
             env: {
-                USER: 'h3players_bot2',
+                USER: 'h3players_bot1',
                 LOG_PATH: '/var/www/output/spectator',
             },
         },
