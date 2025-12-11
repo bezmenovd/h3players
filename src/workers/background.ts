00fca312dc0622d2742db65d1752979c7f13c4e6
bezmenovd <d28829192@gmail.com>
2025-12-11 20:02:13 +0400

    rework: rank

--- src/workers/background.ts
+++ src/workers/background.ts
@@ -1,33 +1,14 @@
-import { rankInit, rankListen } from './src/background/rank'
+import { update as updateRank, listen as listenRank } from './src/background/rank'
 import { logger } from './src/services/logger'
-import { createClient } from 'redis'
 import config from '../../config.json'
 
 
 async function main() {
     logger.info('starting..')
 
-    const redis = createClient({
-        socket: {
-            host: 'redis',
-        }
-    })
-
-    await redis.connect()
-
-    const redisSub = createClient({
-        socket: {
-            host: 'redis',
-        }
-    })
-
-    await redisSub.connect()
-
-    if (config.background.rank.init) {
-        rankInit(redis)
-    }
-    if (config.background.rank.listen) {
-        rankListen(redis, redisSub)
+    if (config.background.rank.enabled) {
+        updateRank()
+        listenRank()
     }
 
     
--- src/workers/src/background/rank.ts
+++ src/workers/src/background/rank.ts
@@ -1,122 +1,53 @@
 import { createClient } from "redis";
 import { lobby } from "../services/clickhouse";
-import { logger } from "../services/logger";
 import { throttle } from "../helpers/functions";
-import { datetime } from "../helpers/timestamp";
-import { sleep } from "../helpers/sleep";
+import { Row } from "@clickhouse/client";
+import { logger } from "../services/logger";
 
 
-export async function rankInit(redis: Awaited<ReturnType<typeof createClient>>) {
-    let lastTimestamp = 0
-    let lastId = 0
-
-    while (true) {
-        lastTimestamp = Number(await redis.get('background:rank:init:lastTimestamp'))
-        lastTimestamp = Number.isFinite(lastTimestamp) ? lastTimestamp : 0
-
-        let games = await (await lobby().query({
-            query: `
-                SELECT 
-                    id,
-                    end_timestamp,
-                    host_id,
-                    host_new_rating,
-                    opponent_id,
-                    opponent_new_rating
-                FROM games
-                WHERE end_timestamp >= {start_timestamp:UInt32}
-                ORDER BY end_timestamp ASC
-                LIMIT 100
-            `,
-            query_params: {
-                start_timestamp: lastTimestamp,
-            },
-            format: 'JSONEachRow',
-        })).json<{ 
-            id: number,
-            end_timestamp: number,
-            host_id: number,
-            host_new_rating: number,
-            opponent_id: number,
-            opponent_new_rating: number,
-        }>()
-
-        if (games.length === 0 || lastId === games[games.length-1].id) {
-            logger.info(`rank: done`)
-            break
+export async function update() {
+    const redis = createClient({
+        socket: {
+            host: 'redis',
         }
+    })
 
-        games.forEach(g => {
-            redis.zAdd('rank', { value: String(g.host_id), score: g.host_new_rating })
-            redis.zAdd('rank', { value: String(g.opponent_id), score: g.opponent_new_rating })
-        })
+    await redis.connect()
 
-        lastId = games[games.length-1].id
+    let gamesStream = (await lobby().query({
+        query: `
+            SELECT 
+                player_id,
+                argMax(player_new_rating, end_timestamp) AS last_rating
+            FROM games_v
+            GROUP BY player_id
+        `,
+        format: 'JSONEachRow',
+    })).stream()
 
-        logger.info(`rank: init at ${ datetime.from(games[games.length-1].end_timestamp)}`)
-
-        await redis.set('background:rank:init:lastTimestamp', String(games[games.length-1].end_timestamp))
-
-        await sleep(100)
+    for await (const rows of gamesStream) {
+        await Promise.all(rows.map(async (row: Row) => {
+            let item = row.json() as { player_id: number, last_rating: number }
+            return redis.zAdd('rank', { value: String(item.player_id), score: item.last_rating })
+        }))
     }
+
+    redis.destroy()
+
+    logger.info('rank updated')
 }
 
-export async function rankListen(redis: Awaited<ReturnType<typeof createClient>>, redisSub: Awaited<ReturnType<typeof createClient>>) {
-    const update = throttle(async () => {
-        let lastTimestamp = 0
-        let lastId = 0
+export async function listen() {
+    const updateThrottled = throttle(update, 10000)
 
-        while (true) {
-            lastTimestamp = Number(await redis.get('background:rank:init:lastTimestamp'))
-            lastTimestamp = Number.isFinite(lastTimestamp) ? lastTimestamp : 0
-    
-            let games = await (await lobby().query({
-                query: `
-                    SELECT 
-                        id,
-                        end_timestamp,
-                        host_id,
-                        host_new_rating,
-                        opponent_id,
-                        opponent_new_rating
-                    FROM games
-                    WHERE end_timestamp >= {start_timestamp:UInt32}
-                    ORDER BY end_timestamp ASC
-                    LIMIT 100
-                `,
-                query_params: {
-                    start_timestamp: lastTimestamp,
-                },
-                format: 'JSONEachRow',
-            })).json<{ 
-                id: number,
-                end_timestamp: number,
-                host_id: number,
-                host_new_rating: number,
-                opponent_id: number,
-                opponent_new_rating: number,
-            }>()
-
-            if (games.length === 0 || lastId === games[games.length-1].id) {
-                break
-            }
-    
-            games.forEach(g => {
-                redis.zAdd('rank', { value: String(g.host_id), score: g.host_new_rating })
-                redis.zAdd('rank', { value: String(g.opponent_id), score: g.opponent_new_rating })
-            })
-
-            lastId = games[games.length-1].id
-
-            await redis.set('background:rank:init:lastTimestamp', String(games[games.length-1].end_timestamp))
-
-            await sleep(100)
+    const redisSub = createClient({
+        socket: {
+            host: 'redis',
         }
+    })
 
+    await redisSub.connect()
 
-        logger.info(`rank: updated to ${ datetime.from(lastTimestamp) }`)
-    }, 500)
-
-    redisSub.subscribe('live:processor:games', update)
+    redisSub.subscribe('live:processor:games', () => updateThrottled())
 }
 
